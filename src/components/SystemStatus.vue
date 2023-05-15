f06c799e2e91cd10faf9d5b8c45599f7cd0d3efe
Yu_wen <a3538772153@163.com>
2023-05-15 09:27:05 +0800

    6

--- src/components/SystemStatus.vue
+++ src/components/SystemStatus.vue
@@ -1,71 +1,82 @@
 <template>
   <div>
-    <h1>System Information</h1>
-    <div>
-      <h2>CPU Usage</h2>
-      <el-progress type="circle" :percentage="cpuUsage"></el-progress>
-    </div>
-    <div>
-      <h2>Memory Usage</h2>
-      <el-progress type="circle" :percentage="memoryUsage"></el-progress>
-    </div>
+    <el-progress
+      v-for="(item, index) in systemInfo"
+      :key="index"
+      :percentage="item.percentage"
+      :text-inside="true"
+      :stroke-width="18"
+      type="circle"
+      :color="getProgressBarColor(item.percentage)"
+    >
+      {{ item.label }}
+    </el-progress>
   </div>
 </template>
 
-<script lang="ts">
-import { ref, onMounted, onUnmounted } from "vue";
+<script>
 import axios from "axios";
-import { ElProgress } from "element-plus";
 
 export default {
-  components: {
-    ElProgress,
-  },
-  setup() {
-    const cpuUsage = ref<number>(0);
-    const memoryUsage = ref<number>(0);
-
-    let intervalId: number;
-
-    const fetchSystemInfo = async () => {
-      const response = await axios.get("/api/systeminfo");
-      const data = response.data;
-      cpuUsage.value = data.cpu.currentload;
-      memoryUsage.value = (data.memory.used / data.memory.total) * 100;
-    };
-
-    onMounted(() => {
-      fetchSystemInfo();
-      intervalId = setInterval(fetchSystemInfo, 5000);
-    });
-
-    onUnmounted(() => {
-      clearInterval(intervalId);
-    });
-
+  data() {
     return {
-      cpuUsage,
-      memoryUsage,
+      systemInfo: [
+        { label: "CPU", percentage: 0 },
+        { label: "Memory", percentage: 0 },
+        { label: "Network", percentage: 0 },
+        { label: "Disk", percentage: 0 },
+      ],
+      statusColors: {
+        success: 30,
+        warning: 60,
+        danger: 100,
+      },
     };
+  },
+  created() {
+    this.getSystemInfo();
+    setInterval(this.getSystemInfo, 5000);
+  },
+  methods: {
+    getProgressBarColor(percentage) {
+      if (percentage <= this.statusColors.success) {
+        return "success";
+      } else if (percentage <= this.statusColors.warning) {
+        return "warning";
+      } else {
+        return "danger";
+      }
+    },
+    getSystemInfo: async function () {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/systeminfo"
+        ); // 根据实际的后端路由进行修改
+        const { cpu, diskLayout, memory, networkStats } = response.data;
+
+        this.systemInfo = [
+          {
+            label: "CPU",
+            percentage: parseFloat(cpu.currentload),
+          },
+          {
+            label: "Memory",
+            percentage:
+              (parseFloat(memory.used) / parseFloat(memory.total)) * 100,
+          },
+          {
+            label: "Network",
+            percentage: parseFloat(networkStats[0].tx_sec) / 1000, // 以 KB/s 为单位显示
+          },
+          {
+            label: "Disk",
+            percentage: parseFloat(diskLayout[0].use),
+          },
+        ];
+      } catch (error) {
+        console.error("获取系统信息出错：", error);
+      }
+    },
   },
 };
 </script>
-
-<style scoped>
-h1 {
-  text-align: center;
-  margin-bottom: 50px;
-}
-
-div {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 80vh;
-}
-
-h2 {
-  text-align: center;
-  margin-bottom: 20px;
-}
-</style>
