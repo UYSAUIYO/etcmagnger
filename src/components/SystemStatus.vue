83c923f4a8dff6e16f304dbb051975982ef1e942
Yu_wen <a3538772153@163.com>
2023-05-15 10:03:25 +0800

    8

--- src/components/SystemStatus.vue
+++ src/components/SystemStatus.vue
@@ -6,7 +6,7 @@
       :percentage="item.percentage"
       :text-inside="true"
       :stroke-width="18"
-      :type="getProgressBarType(item.percentage)"
+      type="circle"
       :color="getProgressBarColor(item.percentage)"
     >
       {{ item.label }}
@@ -26,7 +26,7 @@ export default {
         { label: "Network", percentage: 0 },
         { label: "Disk", percentage: 0 },
       ],
-      statusThresholds: {
+      statusColors: {
         success: 30,
         warning: 60,
         danger: 100,
@@ -39,49 +39,40 @@ export default {
   },
   methods: {
     getProgressBarColor(percentage) {
-      if (percentage <= this.statusThresholds.success) {
+      if (percentage <= this.statusColors.success) {
         return "success";
-      } else if (percentage <= this.statusThresholds.warning) {
+      } else if (percentage <= this.statusColors.warning) {
         return "warning";
       } else {
         return "danger";
       }
     },
-    getProgressBarType(percentage) {
-      if (percentage <= this.statusThresholds.warning) {
-        return "circle";
-      } else {
-        return "dashboard";
-      }
-    },
-    async getSystemInfo() {
+    getSystemInfo: async function () {
       try {
         const response = await axios.get(
           "http://localhost:3000/api/systeminfo"
         ); // 根据实际的后端路由进行修改
         const { cpu, diskLayout, memory, networkStats } = response.data;
 
-        if (cpu && cpu.currentload !== undefined) {
-          this.systemInfo = [
-            {
-              label: "CPU",
-              percentage: parseFloat(cpu.currentload),
-            },
-            {
-              label: "Memory",
-              percentage:
-                (parseFloat(memory.used) / parseFloat(memory.total)) * 100,
-            },
-            {
-              label: "Network",
-              percentage: parseFloat(networkStats[0].tx_sec) / 1000, // 以 KB/s 为单位显示
-            },
-            {
-              label: "Disk",
-              percentage: parseFloat(diskLayout[0].use),
-            },
-          ];
-        }
+        this.systemInfo = [
+          {
+            label: "CPU",
+            percentage: parseFloat(cpu.currentload),
+          },
+          {
+            label: "Memory",
+            percentage:
+              (parseFloat(memory.used) / parseFloat(memory.total)) * 100,
+          },
+          {
+            label: "Network",
+            percentage: parseFloat(networkStats[0].tx_sec) / 1000, // 以 KB/s 为单位显示
+          },
+          {
+            label: "Disk",
+            percentage: parseFloat(diskLayout[0].use),
+          },
+        ];
       } catch (error) {
         console.error("获取系统信息出错：", error);
       }
